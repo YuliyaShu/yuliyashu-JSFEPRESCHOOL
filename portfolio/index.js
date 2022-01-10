f5fd5d3d3f15e32d1b243afb514f51a54c68462e
YuliyaShu <juliette7373@gmail.com>
2022-01-10 11:48:05 +0300

    docs: modify files according to the cross-check test

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,5 @@
-console.log(
-" fdg\n",
-"fdg\n",
-"fdg\n");
+console.log("1. Valid validator.W3 10/10\n",
+"2. Semantic 20/20\n",
+"3. Layout 45/45\n",
+"4. CSS Requirements 12/12\n",
+"5. CSS Interactivity 20/20");
